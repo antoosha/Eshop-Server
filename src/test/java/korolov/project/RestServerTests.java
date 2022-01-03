922e96d48fc13cb731cf94d118ae80f5dc89d932
Anton Korolov <akorol6969@gmail.com>
2022-01-03 14:51:59 +0100

    trying to add docker

--- src/test/java/korolov/project/RestServerTests.java
+++ src/test/java/korolov/project/RestServerTests.java
@@ -6,10 +6,8 @@ import korolov.project.api.controller.ProductController;
 import korolov.project.api.controller.ShipmentController;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 public class RestServerTests {
